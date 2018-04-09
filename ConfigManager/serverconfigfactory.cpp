552437535cc73df41a29abe3e36ec7f17353390a
undefined <itm0002@hotmail.com>
2018-04-08 22:16:33 -0700

    Finished configmanager

---
+++ ConfigManager/serverconfigfactory.cpp
@@ -0,0 +1,6 @@
+#include "serverconfigfactory.h"
+
+ServerConfigFactory::ServerConfigFactory()
+{
+
+}
---
+++ ConfigManager/serverconfigfactory.h
@@ -0,0 +1,11 @@
+#ifndef SERVERCONFIGFACTORY_H
+#define SERVERCONFIGFACTORY_H
+
+
+class ServerConfigFactory
+{
+public:
+    ServerConfigFactory();
+};
+
+#endif // SERVERCONFIGFACTORY_H
